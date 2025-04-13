b43ac58f158e48e00bea695f0c73723d016dd43e
Ryuhei Mori <mori@math.nagoya-u.ac.jp>
2025-04-13 19:09:53 +0900

    revised

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,20 @@ import tailwindcss from '@tailwindcss/vite';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mori-lab.github.io',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-  }
+  },
+
+  integrations: [mdx()]
 });
